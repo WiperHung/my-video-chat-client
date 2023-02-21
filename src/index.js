b386d34409c196ad47f582dceb6a02bfd721c3e5
Wiper Hung <wiperece@gmail.com>
2023-02-22 01:28:35 +0800

    up11

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles.css";
+import ReactDOM from "react-dom";
+
 import App from "./App";
 import { ContextProvider } from "./Context";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+import "./styles.css";
+
+ReactDOM.render(
   <ContextProvider>
     <App />
-  </ContextProvider>
+  </ContextProvider>,
+  document.getElementById("root")
 );
